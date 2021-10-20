670293b76e5e968804c4d5de566fc88b9a6358e6
Samir Shrestha <shresthasam@deakin.edu.au>
2021-10-20 12:11:03 +1100

    Keep worker device's screen during working

--- app/src/main/java/tnefern/honeybeeframework/worker/WorkerActivity.java
+++ app/src/main/java/tnefern/honeybeeframework/worker/WorkerActivity.java
@@ -18,6 +18,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -33,6 +34,7 @@ public abstract class WorkerActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_worker_layout);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 //		Bundle bundle = getIntent().getExtras();
 //		String workerActivityClass = bundle
 //				.getString(CommonConstants.ACTIVITY_CLASS_NAME);
